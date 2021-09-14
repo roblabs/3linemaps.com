8f0fa4dbd7344bc461310480cbe4fe809ee5d146
RobLabs.com <roblabs@users.noreply.github.com>
2021-09-14 13:00:06 -0700

    3linemaps.com/50.html

--- go.sh
+++ go.sh
@@ -1,1 +1,4 @@
+open http://localhost:4000/50.html
+/Applications/Firefox.app/Contents/MacOS/firefox http://localhost:4000/50.html
+
 docker run --rm -v $(pwd):/srv/jekyll   -p 4000:4000   -it   jekyll/jekyll   jekyll serve --disable-disk-cache
